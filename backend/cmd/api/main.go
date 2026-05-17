49cae77282ccc1220e4d66f132089fc792a9b8ec
Du Jiajun <jiajun_du@outlook.com>
2026-05-17 22:25:18 +0800

    add server initialization with config and graceful shutdown logic

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,5 +1,68 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+
+	"jcourse/config"
+	"jcourse/internal/app"
+	"jcourse/internal/interface/web"
+)
+
+func main() {
+	pflag.String("config", "", "path to config file (env: CONFIG_PATH)")
+	pflag.Parse()
+
+	if err := viper.BindPFlag("config", pflag.Lookup("config")); err != nil {
+		log.Fatalf("bind config flag: %v", err)
+	}
+	if err := viper.BindEnv("config", "CONFIG_PATH"); err != nil {
+		log.Fatalf("bind config env: %v", err)
+	}
+
+	configPath := viper.GetString("config")
+	if configPath == "" {
+		configPath = "config/config.yaml"
+	}
+
+	conf, err := config.Load(configPath)
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	container := app.NewServiceContainer(conf)
+	engine := web.NewRouter(container)
+
+	srv := &http.Server{
+		Addr:    conf.Server.Addr,
+		Handler: engine,
+	}
+
+	go func() {
+		fmt.Printf("server starting on %s\n", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("server forced to shutdown:", err)
+	}
+	fmt.Println("server exited")
 }
--- backend/config/config.go
+++ backend/config/config.go
@@ -1,17 +1,55 @@
 package config
 
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
 type AppConfig struct {
-	Postgres PostgresConfig `yaml:"postgres"`
-	Redis    RedisConfig    `yaml:"redis"`
+	Server   ServerConfig   `mapstructure:"server"`
+	Postgres PostgresConfig `mapstructure:"postgres"`
+	Redis    RedisConfig    `mapstructure:"redis"`
+}
+
+type ServerConfig struct {
+	Addr string `mapstructure:"addr"`
 }
 
 type PostgresConfig struct {
-	DSN string `yaml:"dsn"`
+	DSN string `mapstructure:"dsn"`
 }
 
 type RedisConfig struct {
-	Addr     string `yaml:"addr"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-	DB       int    `yaml:"db"`
+	Addr     string `mapstructure:"addr"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	DB       int    `mapstructure:"db"`
+}
+
+func Load(configPath string) (AppConfig, error) {
+	v := viper.New()
+
+	if configPath != "" {
+		v.SetConfigFile(configPath)
+	} else {
+		v.AddConfigPath(".")
+		v.SetConfigName("config")
+		v.SetConfigType("yaml")
+	}
+
+	v.SetEnvPrefix("JCOURSE")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return AppConfig{}, fmt.Errorf("read config: %w", err)
+	}
+
+	var conf AppConfig
+	if err := v.Unmarshal(&conf); err != nil {
+		return AppConfig{}, fmt.Errorf("unmarshal config: %w", err)
+	}
+	return conf, nil
 }
